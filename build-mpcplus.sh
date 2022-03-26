394c3f14afa1fcc4f50901d8d74de5f039d53ca5
Ronald Record <github@ronrecord.com>
2022-03-26 10:34:49 -0700

    Move mpcplus into subdir

---
+++ build-mpcplus.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#
+# Repo: https://github.com/doctorfree/MusicPlayerPlus
+#
+# Fork of Repo: https://github.com/ncmpcpp/ncmpcpp
+# Original Project: https://rybczak.net/ncmpcpp/
+#
+# To install original ncmpcpp:
+# sudo apt install ncmpcpp
+#
+# To build and install mpcplus from source see the following
+#
+# Dependencies include:
+# sudo apt install libboost
+# sudo apt install libboost-all-dev
+# sudo apt install libmpdclient-dev
+# sudo apt install libcurl4-openssl-dev
+# sudo apt install libfftw3-dev
+# sudo apt install libtag1-dev
+#
+# Configure options include:
+# --enable-outputs        Enable outputs screen [default=no]
+# --enable-visualizer     Enable music visualizer screen [default=no]
+# --enable-clock          Enable clock screen [default=no]
+#
+# Usage: ./build-mpcplus.sh [-i]
+# Where -i indicates install mpcplus after configuring and compiling
+
+usage() {
+    printf "\nUsage: ./build-mpcplus.sh [-aCi] [-p prefix] [-u]"
+    printf "\nWhere:"
+    printf "\n\t-a indicates run autogen script and exit"
+    printf "\n\t-C indicates run autogen, and configure and exit"
+    printf "\n\t-i indicates configure, build, and install"
+    printf "\n\t-p prefix specifies installation prefix (default /usr)"
+    printf "\n\t-u displays this usage message and exits\n"
+    printf "\nNo arguments: configure with prefix=/usr, build\n"
+    exit 1
+}
+
+PROJ=mpcplus
+CONFIGURE_ONLY=
+AUTOGEN_ONLY=
+INSTALL=
+PREFIX=
+while getopts "aCip:u" flag; do
+    case $flag in
+        a)
+            AUTOGEN_ONLY=1
+            ;;
+        C)
+            CONFIGURE_ONLY=1
+            ;;
+        i)
+            INSTALL=1
+            ;;
+        p)
+            PREFIX="$OPTARG"
+            ;;
+        u)
+            usage
+            ;;
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+[ -d ${PROJ} ] || {
+    echo "$PROJ does not exist or is not a directory."
+    echo "Run: git clone https://github.com/doctorfree/MusicPlayerPlus"
+    echo "Exiting"
+    exit 1
+}
+
+cd ${PROJ}
+[ -x ./configure ] || ./autogen.sh
+[ "${AUTOGEN_ONLY}" ] && exit 0
+
+prefix="/usr"
+[ "${PREFIX}" ] && prefix="--prefix=${PREFIX}"
+./configure ${prefix} \
+            --enable-outputs \
+            --enable-clock \
+            --disable-visualizer \
+            --with-taglib
+#           --enable-unicode \
+#           --without-iconv \
+#           --with-curl \
+#           --enable-static-boost
+[ "${CONFIGURE_ONLY}" ] && exit 0
+
+make
+
+[ "${INSTALL}" ] && sudo make install
