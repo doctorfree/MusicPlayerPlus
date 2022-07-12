b948391cd27dceb09c6bfee74cdc381cb872721c
Ronald Record <github@ronrecord.com>
2022-07-12 10:13:59 -0700

    Calculate elapsed time in beet_log.sh called by Beets hook plugin

--- share/scripts/beet_log.sh
+++ share/scripts/beet_log.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
+#
+# beet_log.sh - log beet import times, calculate elapsed time
+#
+# Written 12/Jul/2022 by Ronald Joe Record <ronaldrecord@gmail.com>
+#
+# This script is intended to be called from the Beets hook plugin
+# It is called when a beet import begins and again when the import ends
+# The calculated elapsed time between calls provides the user with
+# the length of time the import took. Large music libraries can take
+# many hours to import. The length of import time can vary depending
+# upon which plugins are enabled and the settings for those plugins.
+#
+# If import times are unreasonably long, try removing/disabling/reconfiguring
+# some plugins. Plugins that may increase import times include the
+# acousticbrainz and fetchart plugins.
+#
+# Importing "as-is" can avoid long import times.
+#
+# See ~/.config/beets/config.yaml for the hook plugin configuration
 
-LOG="${HOME}/.config/beets/beet_log.txt"
-DATE=`date`
+LOG="${HOME}/.config/beets/import_time.txt"
+DATE=$(date "+%Y/%m/%d at %H:%M:%S")
+SECONDS=$(date +%s)
 
 [ -f ${LOG} ] || touch ${LOG}
-echo ${DATE} >> ${LOG}
-echo $* >> ${LOG}
+
+PREVSECS=$(tail -1 ${LOG} | awk ' { print $1 } ')
+if [ "${PREVSECS}" ]
+then
+  ELAPSECS=$(( SECONDS - PREVSECS ))
+  ELAPSED=`eval "echo elapsed time: $(date -ud "@$ELAPSECS" +'$((%s/3600/24)) days %H hr %M min %S sec')"`
+else
+  ELAPSED="begin logging import times"
+fi
+
+printf "${SECONDS} on ${DATE} $* , ${ELAPSED}\n" >> ${LOG}
