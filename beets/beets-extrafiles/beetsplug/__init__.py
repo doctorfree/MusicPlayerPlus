d06bd225988ea0fb5387258823d1dc9743f5e603
Ronald Record <github@ronrecord.com>
2022-07-16 11:05:15 -0700

    Delete beets-extrafiles plugin, patch extrafiles.py in mpcinit

--- beets/beets-extrafiles/beetsplug/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Package for the beets-extrafiles plugin."""
-__import__('pkg_resources').declare_namespace(__name__)
--- beets/beets-extrafiles/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for the beets-extrafiles plugin."""
