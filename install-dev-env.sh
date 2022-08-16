00f30b8a6acc868e91205583ba9414462de308b3
Ronald Record <github@ronrecord.com>
2022-08-16 12:31:35 -0700

    Use ffmpeg-full on Arch Linux

--- install-dev-env.sh
+++ install-dev-env.sh
@@ -28,9 +28,10 @@ then
 else
   if [ "${arch}" ]
   then
-    PKGS="base-devel eigen fftw clang ffmpeg libsamplerate taglib chromaprint \
-          libmpdclient boost-libs iniparser libyaml swig python alsa-lib \
-          ncurses readline libpulse libcurl-compat sqlite qt5-base qt5-tools"
+    PKGS="base-devel eigen fftw clang ffmpeg-full libsamplerate taglib \
+          chromaprint libmpdclient boost-libs iniparser libyaml swig python \
+          alsa-lib ncurses readline libpulse libcurl-compat sqlite qt5-base \
+          qt5-tools"
     if [ "$1" == "-r" ]
     then
       sudo pacman -Rs ${PKGS}
