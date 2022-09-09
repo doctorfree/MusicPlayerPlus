da73baee5461bd2ed6aea3ab31cdf3840964edfa
Ronald Record <github@ronrecord.com>
2022-09-09 15:02:49 -0700

    Merge previous ncmpcpp config settings into mpcplus/config

---
+++ share/scripts/set_prev_ncmpcpp.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# set_prev_ncmpcpp.sh - merge previously configured ncmpcpp user preferences
+#                       with $HOME/.config/mpcplus/config
+
+USERCONF="${HOME}/.config"
+NCM_USER_CONF="${USERCONF}/ncmpcpp/config"
+MPC_USER_CONF="${USERCONF}/mpcplus/config"
+
+# Syncing previous user configured ncmpcpp key bindings not yet implemented
+NCM_USER_BIND="${USERCONF}/ncmpcpp/bindings"
+MPC_USER_BIND="${USERCONF}/mpcplus/bindings"
+
+excluded_ncm_opts=("mpd_music_dir" "visualizer_in_stereo" \
+                   "visualizer_type" "message_delay_time")
+previous_comments="## Previously configured ncmpcpp user preferences"
+
+[ -f "${NCM_USER_CONF}" ] && [ -f "${MPC_USER_CONF}" ] && {
+  # Only allowed to be run once if changes to mpcplus/config were made
+  grep "${previous_comments}" "${MPC_USER_CONF}" > /dev/null && exit 0
+  grep -v ^# "${NCM_USER_CONF}" | while read ncmconf
+  do
+    option=`echo "${ncmconf}" | awk -F '=' ' { print $1 } '`
+    option=`echo ${option} | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+    [[ " ${excluded_ncm_opts[*]} " =~ " ${option} " ]] && continue
+    [ "${option}" ] && {
+      oval=`echo "${ncmconf}" | awk -F '=' ' { print $2 } '`
+      oval=`echo ${oval} | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+      [ "${oval}" ] && {
+        mval=`grep "^${option} " "${MPC_USER_CONF}" | \
+                awk -F '=' ' { print $2 } '`
+        mval=`echo ${mval} | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+        [ "${mval}" == "${oval}" ] || {
+          # Use the previously configured value for this option
+          grep -v "^${option} " "${MPC_USER_CONF}" > /tmp/ncm$$
+          grep "${previous_comments}" /tmp/ncm$$ > /dev/null || {
+            echo "${previous_comments}" >> /tmp/ncm$$
+          }
+          echo "${ncmconf}" >> /tmp/ncm$$
+          cp /tmp/ncm$$ "${MPC_USER_CONF}"
+          rm -f /tmp/ncm$$
+        }
+      }
+    }
+  done
+}
+
+[ -f "${NCM_USER_BIND}" ] && {
+  numb=`grep -v ^# "${NCM_USER_BIND}" | grep -v -e '^[[:space:]]*$' | wc -l`
+  [ ${numb} -gt 0 ] && {
+    if [ -f "${MPC_USER_BIND}" ]
+    then
+      printf "\nIntegration of previously configured ncmpcpp bindings is not"
+      printf "yet implemented.\nA manual merge of the configured key bindings"
+      printf "in:\n\n${NCM_USER_BIND} and ${MPC_USER_BIND}"
+      printf "\n\nwill be necessary.\n"
+    else
+      [ -d "${USERCONF}/mpcplus" ] || mkdir -p "${USERCONF}/mpcplus"
+      cp "${NCM_USER_BIND}" "${MPC_USER_BIND}"
+    fi
+  }
+}
+
+exit 0
