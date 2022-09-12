9ae0630937403d7a6ed92d8bd49acf84ebe14951
Ronald Record <github@ronrecord.com>
2022-09-12 09:34:01 -0700

    Add readline-devel to CentOS build dependencies

--- scripts/install-dev-env.sh
+++ scripts/install-dev-env.sh
@@ -96,7 +96,7 @@ else
         PKGS="alsa-lib-devel ncurses-devel fftw3-devel qt5-qtbase-devel \
           pulseaudio-libs-devel libtool automake iniparser-devel SDL2-devel \
           boost-devel eigen3-devel libyaml-devel clang-devel swig \
-          libchromaprint-devel python3-devel python3-yaml \
+          readline-devel libchromaprint-devel python3-devel python3-yaml \
           python3-six sqlite-devel pandoc zip libmpdclient-devel taglib-devel"
         if [ "$1" == "-r" ]
         then
