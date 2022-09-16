98f9e5ce08de35b6692261d5341177cd0575f3dd
Ronald Record <github@ronrecord.com>
2022-09-16 08:57:57 -0700

    Add script to add/remove RPM Fusion repos

---
+++ scripts/rpmfusion.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# rpmfusion.sh - add or remove RPM Fusion repos on Fedora/CentOS
+
+arch=
+centos=
+debian=
+fedora=
+[ -f /etc/os-release ] && . /etc/os-release
+[ "${ID_LIKE}" == "debian" ] && debian=1
+[ "${ID}" == "arch" ] && arch=1
+[ "${ID}" == "centos" ] && centos=1
+[ "${ID}" == "fedora" ] && fedora=1
+[ "${debian}" ] || [ -f /etc/debian_version ] && debian=1
+
+if [ "${debian}" ]
+then
+  echo "This script is only relevant on Fedora and CentOS"
+  exit 1
+else
+  if [ "${arch}" ]
+  then
+    echo "This script is only relevant on Fedora and CentOS"
+    exit 1
+  else
+    have_dnf=`type -p dnf`
+    if [ "${have_dnf}" ]
+    then
+      PINS=dnf
+    else
+      PINS=yum
+    fi
+    sudo ${PINS} makecache
+    if [ "${fedora}" ]
+    then
+      FEDVER=`rpm -E %fedora`
+      FUSION="https://download1.rpmfusion.org"
+      FREE="free/fedora"
+      NONFREE="nonfree/fedora"
+      RELRPM="rpmfusion-free-release-${FEDVER}.noarch.rpm"
+      NONRPM="rpmfusion-nonfree-release-${FEDVER}.noarch.rpm"
+      if [ "$1" == "-r" ]
+      then
+        sudo ${PINS} -y remove ${FUSION}/${NONFREE}/${NONRPM}
+        sudo ${PINS} -y remove ${FUSION}/${FREE}/${RELRPM}
+      else
+        sudo ${PINS} -y install ${FUSION}/${FREE}/${RELRPM}
+        sudo ${PINS} -y install ${FUSION}/${NONFREE}/${NONRPM}
+        sudo ${PINS} -y update
+      fi
+    else
+      if [ "${centos}" ]
+      then
+        CENVER=`rpm -E %centos`
+        FUSION="https://download1.rpmfusion.org"
+        FREE="free/el"
+        NONFREE="nonfree/el"
+        RELRPM="rpmfusion-free-release-${CENVER}.noarch.rpm"
+        NONRPM="rpmfusion-nonfree-release-${CENVER}.noarch.rpm"
+        if [ "$1" == "-r" ]
+        then
+          sudo ${PINS} -y remove ${FUSION}/${NONFREE}/${NONRPM}
+          sudo ${PINS} -y remove ${FUSION}/${FREE}/${RELRPM}
+        else
+          sudo ${PINS} -y install epel-release
+          sudo ${PINS} config-manager --set-enabled powertools
+          sudo ${PINS} -y localinstall --nogpgcheck ${FUSION}/${FREE}/${RELRPM}
+          sudo ${PINS} -y localinstall --nogpgcheck ${FUSION}/${NONFREE}/${NONRPM}
+          sudo ${PINS} -y update
+        fi
+      else
+        echo "Unrecognized operating system"
+      fi
+    fi
+  fi
+fi
