178c51c30dba37396e2bc6f160dcac6ae945cedd
Ronald Record <github@ronrecord.com>
2022-12-12 13:57:54 -0800

    Update dev env script to build updated mppcava

--- scripts/install-dev-env.sh
+++ scripts/install-dev-env.sh
@@ -18,11 +18,11 @@ then
   PKGS="build-essential libeigen3-dev libfftw3-dev clang ffmpeg \
         libavcodec-dev libavformat-dev libavutil-dev libswresample-dev \
         libsamplerate0-dev libtag1-dev libchromaprint-dev libmpdclient-dev \
-        autotools-dev autoconf libtool libboost-all-dev fftw-dev \
-        libiniparser-dev libyaml-dev swig python3-dev pkg-config \
-        libncurses-dev libasound2-dev libreadline-dev libpulse-dev \
-        libcurl4-openssl-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools \
-        libavfilter-dev libavdevice-dev libsqlite3-dev"
+        autotools-dev autoconf autoconf-archive libtool libboost-all-dev \
+        fftw-dev libsdl2-2.0-0 libsdl2-dev libiniparser-dev libyaml-dev swig \
+        python3-dev pkg-config libncurses-dev libasound2-dev libreadline-dev \
+        libpulse-dev libcurl4-openssl-dev qtbase5-dev qtchooser qt5-qmake \
+        qtbase5-dev-tools libavfilter-dev libavdevice-dev libsqlite3-dev"
   if [ "$1" == "-r" ]
   then
     sudo apt remove ${PKGS}
